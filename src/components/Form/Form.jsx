c5c3a52f463d295db41c6c5c478bd394f1afc220
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2023-01-17 23:19:15 +0200

    add

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,7 @@
 import { Component } from "react";
 
+import styles from "./Form.module.css"
+
 export class Form extends Component {
     state = {
         name: '',
@@ -8,16 +10,15 @@ export class Form extends Component {
     render() {
         return (
             <form>
-                <label>
-                    Name
-                    <input
-                        type="text"
-                        name="name"
-                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                        required
-                    />
-                </label>
+                <label htmlFor="name">Name</label>
+
+                <input
+                    type="text"
+                    name="name"
+                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                    required
+                />
 
                 <button type="submit">Add contact</button>
             </form>
